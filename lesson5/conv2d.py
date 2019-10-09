ae7a6e5441dce94b5065cbd96ee93f96e775a3ff
wer010 <yuri924@163.com>
2019-10-09 21:05:04 +0800

    complete the 'continue train' code

--- lesson5/conv2d.py
+++ lesson5/conv2d.py
@@ -4,13 +4,19 @@ import numpy as np
 kernel = tf.constant(1.0, shape=[6,6,3,1])
 
 x3 = tf.constant(1.0, shape=[1,10,10,3])
+y3 = tf.constant(1.0, shape=[1,5,5,1])
 
-y = tf.nn.conv2d(x3, kernel, strides=[1,1,1,1], padding="SAME")
-# x3t = tf.nn.conv2d_transpose(y,kernel,output_shape=[1,5,5,3], strides=[1,2,2,1],padding="SAME")
+y = tf.nn.conv2d(x3, kernel, strides=[1,2,2,1], padding="SAME")
+x3t = tf.nn.conv2d_transpose(y3,kernel,output_shape=[1,10,10,3], strides=[1,2,2,1],padding="SAME")
 
-with tf.Session() as sess:
-    result = sess.run(y)
-    npr = np.asarray(result)
+def print_shape(x):
+    npr = np.asarray(x)
     print(npr.shape)
     print(npr)
-    # print(sess.run(x3t))
+
+with tf.Session() as sess:
+    x = sess.run(y)
+    print_shape(x)
+    x = sess.run(x3t)
+    print_shape(x)
+
